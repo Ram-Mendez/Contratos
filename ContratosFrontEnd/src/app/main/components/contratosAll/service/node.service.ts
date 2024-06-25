c222b61fc8dbe0370e77376c9a1fe68e2acf4f87
RamMendez <balaramji95@gmail.com>
2024-06-25 15:57:34 +0200

    inventory developments, but detalles inventario is gray

--- ContratosFrontEnd/src/app/main/components/contratosAll/service/node.service.ts
+++ ContratosFrontEnd/src/app/main/components/contratosAll/service/node.service.ts
@@ -1,15 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {NodeInput} from "./node-input";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NodeService {
   apiUrl = 'http://localhost:8080/contrato/';
+  apiUrl2 = 'http://localhost:8080/nodes/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  createNode(node: any) {
-    return this.http.post('/api/node', node);
+  // mapear Node con NodeInputdto
+  createNode(idContrato: any, node: any) {
+    return this.http.post(this.apiUrl + idContrato + '/nodes', node);
+  }
+
+  getNodes(idContrato: any, parentId?: any) {
+    let params = {};
+    if (parentId) {
+      params = {parentId: parentId};
+    }
+    return this.http.get(this.apiUrl + idContrato + '/nodes', {params: params});
+  }
+
+  deleteNode(id: number) {
+    return this.http.delete(this.apiUrl2 + id);
   }
 }
